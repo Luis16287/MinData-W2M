5bc2e381f9fbeb165ea359b63da180ac3c195b81
Luis Ariel Farfan Huebra <luis.exactas@gmail.com>
2022-01-07 13:31:21 -0300

    Comments removed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  // it(`should have as title 'MinData-W2M'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('MinData-W2M');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('MinData-W2M app is running!');
-  // });
 });
